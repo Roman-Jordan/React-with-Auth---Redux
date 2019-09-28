afe024aa7fea6dfe73a1380a0b3f8f44ed515b92
roman-jordan <rjordan1352@gmail.com>
2019-09-27 22:55:19 -0700

    Login Working

--- src/App.js
+++ src/App.js
@@ -4,34 +4,17 @@ import {Route} from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import PublicMainView from './components/public'
 import PrivateMainView from './components/private'
-import { axiosWithAuth } from './util/axiosWithAuth';
+import { axiosWithAuth,loginHandler } from './util/axiosWithAuth';
 
 function App() {
   const [loggedIn,setLoggedIn] = useState(false)
   const [error,setError] = useState()
 
-  useEffect(()=>{
-    if(!loggedIn){
-      axiosWithAuth().post('/login',{username:"Lambda School",password:"i<3Lambd4"})
-      .then(res=>
-        res.status === 200 
-          && localStorage.setItem("token",res.data)
-          && setLoggedIn(true)
-          && setError()
-      )
-      .catch(err=>{console.log(err)})
-    }
-  },[loggedIn])
   
-  setTimeout(()=>{
-    
-    setLoggedIn(!loggedIn)
-  },5000)
 
-  // console.log(loggedIn)
-  // console.log(localStorage)
   return (
     <div className="App">
+      <LoginForm />
       {error && <p>{error}</p>}
       {loggedIn ? <PrivateRoute path="/" component={PrivateMainView}  />
       :<Route path="/" component={PublicMainView} />}
@@ -40,3 +23,24 @@ function App() {
 }
 
 export default App;
+
+export const LoginForm = () =>{
+  let [user,setUser] = useState({})
+  let username = user.username ? user.username:'';
+  let password = user.password ? user.password:'';
+  const change= (e) =>{
+    setUser({...user,[e.target.name]:e.target.value})
+  }
+  const onSubmit= (e) =>{
+    e.preventDefault()
+    console.log('HERE',loginHandler(user))
+  }
+  console.log(user)
+  return(
+    <form onSubmit={onSubmit}>
+      <input onChange={change} name="username" placeholder="Username" value={username} type="text"/>
+      <input onChange={change} name="password" placeholder="Password" value={password} type="password" />
+      <input type="submit" />
+    </form>
+  )
+}
--- src/util/axiosWithAuth.js
+++ src/util/axiosWithAuth.js
@@ -1,11 +1,31 @@
 import axios from 'axios'
+const client = process.env.REACT_APP_CLIENT_ID
+const secret = process.env.REACT_APP_CLIENT_SECRET
+
+const api_key = btoa(`${client}:${secret}`);
+const baseURL = 'http://localhost:2019'
 
 export const axiosWithAuth = () =>{
     const token = localStorage.getItem("token")
+    
     return axios.create({
-        baseURL: 'http://localhost:5000/api',
+        baseURL: baseURL,
+        timeout: 1000,
         headers:{
-            Authorization: token
+            Authorization: `Bearer ${token}`
         }
     })
+}
+
+
+export const loginHandler = (u) =>  {
+    axios
+      .post(`${baseURL}/oauth/token`, `grant_type=password&username=${u.username}&password=${u.password}`,{
+        headers:{
+          Authorization:`Basic ${api_key}`,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      })
+      .then(res=>localStorage.setItem("token",res.data.access_token))
+      .catch(err => console.log(err))  
 }
