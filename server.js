fd472917bc43c66762836bef1bef54d1fa66bdbf
roman-jordan <rjordan1352@gmail.com>
2019-10-03 23:07:48 -0700

    Server Up and running

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+require('dotenv').config()
 const server = express()
-require('dotenv')
 
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 5000 ;
 
 
 server.listen(port, () =>{
-    console.log(`\n Server listening on port: ${port} ** \n`)
+    console.log(`\n ${port} Server listening on port: ${port} ** \n`)
 })
+  
+
