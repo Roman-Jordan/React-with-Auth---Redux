d2e297e5e115644f89777651d2932780acccc397
roman-jordan <rjordan1352@gmail.com>
2019-10-03 23:37:38 -0700

    Check out npm start

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 require('dotenv').config()
 const server = express()
-
+const chalk = require('chalk');
+const log = console.log;
 
 const port = process.env.PORT || 5000 ;
 
-
 server.listen(port, () =>{
-    console.log(`\n ${port} Server listening on port: ${port} ** \n`)
-})
+log(`\n${chalk.green('*************************************')} 
+    Server listening on port: ${chalk.blue(port)} 
+${chalk.green('*************************************')}\n`)
+
+log(`     ♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪\n`)})
   
 
