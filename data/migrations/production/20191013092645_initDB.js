5220c33f8e1d2634f1f196dc9c5e77f839e83968
roman-jordan <rjordan1352@gmail.com>
2019-10-14 09:43:50 -0700

    Added Becrypt and Decreased zip query time

--- data/migrations/production/20191013092645_initDB.js
+++ data/migrations/production/20191013092645_initDB.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("us_location_data", col => {
     col.increments().primary();
-    col.integer("zip", 5).notNullable();
+    col.integer("zip", 5).notNullable().index();
     col.text("zipType", 1).notNullable();
     col.string("city", 30).notNullable();
     col.string("county", 50).notNullable();
@@ -13,6 +13,7 @@ exports.up = function(knex) {
     col.text("DST", 1).notNullable();
     col.float("latitude", 14, 10).notNullable();
     col.float("longitude", 14, 10).notNullable();
+
   });
 };
 exports.down = function(knex) {
