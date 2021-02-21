2bc96cca99f4cd1854e14a9ca7b4eb69446c9e02
roman-jordan <rjordan1352@gmail.com>
2021-02-20 16:44:21 -0800

    Cleaned up linting

--- data/migrations/20191031210602_users.js
+++ data/migrations/20191031210602_users.js
@@ -1,16 +1,11 @@
-exports.up = function(knex) {
-  return knex.schema.createTable("users", col => {
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (col) => {
     col.increments();
-    col
-      .string("email", 100)
-      .unique()
-      .notNullable();
-    col
-      .string("password", 128)
-      .notNullable();
+    col.string("email", 100).unique().notNullable();
+    col.string("password", 128).notNullable();
   });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists("users");
 };
--- data/seeds/000-cleaner.js
+++ data/seeds/000-cleaner.js
@@ -1,10 +1,10 @@
 const cleaner = require("knex-cleaner");
 
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   const clean = await cleaner.clean(knex, {
     mode: "truncate",
     restartIdentity: true,
-    ignoreTables: ["knex_migrations", "knex_migrations_lock"]
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"],
   });
 
   return clean;
