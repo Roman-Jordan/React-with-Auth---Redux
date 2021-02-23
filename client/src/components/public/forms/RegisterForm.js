87f3d26df2fb8dfb4e098c6f1b02a07a72fb91c3
Roman Jordan <rjordan1352@gmail.com>
2021-02-22 21:40:15 -0800

    removed unneeded data

--- client/src/components/public/forms/RegisterForm.js
+++ client/src/components/public/forms/RegisterForm.js
@@ -14,15 +14,15 @@ const RegisterForm = (props) => {
 
   const onSubmit = e => {
     e.preventDefault();
-      //user.email 
-      //&& user.password 
-     // && props.loginHandler(user,props);
+    //user.email 
+    //&& user.password 
+    // && props.loginHandler(user,props);
   };
 
   return (
     <div id="registerForm">
       <h1>Register</h1>
-       <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit}>
         <input
           onChange={change}
           name="email"
@@ -38,7 +38,7 @@ const RegisterForm = (props) => {
           type="password"
         />
 
-        <input type="submit" />
+        <button type="submit" >Submit</button>
       </form>
     </div>
   );
@@ -52,150 +52,3 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps, null)(RegisterForm);
 
-export const RoleSelector = (props) => {
-  const { userRoles, change } = props;
-  return (
-    <div className="role-container">
-      {userRoles &&
-        userRoles.map((role, i) => {
-          return (
-            <div className="role-selector-radio" key={i}>
-              <button name="selected-roll" value={role} onClick={change}>
-                {role}
-              </button>
-            </div>
-          );
-        })}
-    </div>
-  );
-};
-
-export const Provider = () => {
-  return (
-    <>
-      <p>Hello from Provider</p>
-
-      <SharedFeilds />
-      <input type="text" name="industry" />
-    </>
-  );
-};
-
-export const Consumer = () => {
-  return (
-    <>
-      <SharedFeilds />
-    </>
-  );
-};
-
-export const Company = () => {
-  return (
-    <>
-      <SharedFeilds />
-      <input type="text" name="company" placeholder="Company" />
-      <input type="text" placeholder="Industry" />
-      <input type="text" placeholder="Primary Address" />
-      <input type="number" placeholder="Employee Count" />
-    </>
-    // First Name,	R/S, TEXT --
-    // Last Name	  R/S  TEXT --
-    // Company	    R    TEXT --
-    // Industry	    O    TEXT --
-    // Email	      R/S
-    // Employees	  R    NUMB
-    // Direct phone	R    NUMB
-    // Mobile phone	O    NUMB
-
-    // Website	    O    URL-
-    // Address      R
-
-    // Descrip      O    LONG
-  );
-};
-
-export const Sponsor = () => {
-  return (
-    <>
-      <SharedFeilds />
-    </>
-  );
-};
-
-export const SharedFeilds = () => {
-  return (
-    <>
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="email" placeholder="Email" />
-    </>
-  );
-};
-
-//******************************************** */
-// PROVIDER:
-
-// First Name,	R/S, TEXT --
-// Last Name	  R/S  TEXT --
-// Company	    R    TEXT --
-// Industry	    O    TEXT --
-// Email	      R/S
-// Employees	  R    NUMB
-// Direct phone	R    NUMB
-// Mobile phone	O    NUMB
-
-// Website	    O    URL-
-// Address      R
-
-// Descrip      O    LONG
-//******************************************** */
-// CONSUMER
-
-// First Name,	R/S, TEXT
-// Last Name	  R/S  TEXT
-// Company	    R    TEXT
-// Industry	    O    TEXT
-// Email	      R/S
-// Employees	  R    NUMB
-// Direct phone	R    NUMB
-// Mobile phone	O    NUMB
-
-// Website	    O    URL-
-// Address      R
-
-// Descrip      O    LONG
-
-//******************************************** */
-
-// COMPANIES:
-
-// First Name,	R/S, TEXT --
-// Last Name	  R/S  TEXT --
-// Company	    R    TEXT --
-// Industry	    O    TEXT --
-// Email	      R/S
-// Employees	  R    NUMB
-// Direct phone	R    NUMB
-// Mobile phone	O    NUMB
-
-// Website	    O    URL-
-// Address      R
-
-// Descrip      O    LONG
-
-//******************************************** */
-// SPONSOR:
-
-// First Name,	R/S, TEXT
-// Last Name	  R/S  TEXT
-// Company	    R    TEXT
-// Industry	    O    TEXT
-// Email	      R/S
-// Employees	  R    NUMB
-// Direct phone	R    NUMB
-// Mobile phone	O    NUMB
-
-// Website	    O    URL-
-// Address      R
-
-// Descrip      O    LONG
